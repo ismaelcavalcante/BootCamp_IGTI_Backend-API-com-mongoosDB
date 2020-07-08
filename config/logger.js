fd6a40d687a63eabf8b0d899317050f84b2c4570
ismaelcavalcante <ismael.agriroo@gmail.com>
2020-07-08 16:21:06 -0400

    Finalizando configuração de conexão

--- config/logger.js
+++ config/logger.js
@@ -2,8 +2,6 @@ import winston from 'winston';
 import winstondb from 'winston-mongodb';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 const { combine, timestamp, label, printf } = winston.format;
 
 const { createLogger, transports } = winston;
@@ -12,20 +10,22 @@ const myFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level}: ${message}`;
 });
 
+dotenv.config();
+
 const logger = createLogger({
   transports: [
     new transports.Console(),
     new transports.MongoDB({
-        level: 'info',
-        db: `mongodb+srv://ismael:is23568974@bootcampfullstack-dwnrk.mongodb.net/grades?retryWrites=true&w=majority`,
-        collection: 'logs_grades',
-        capped: true,
-        cappedMax: 20,
-        options: {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        },
-      })
+      level: 'info',
+      db: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSW}@bootcampfullstack-dwnrk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      collection: 'logs_grades',
+      capped: true,
+      cappedMax: 20,
+      options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    }),
   ],
   format: combine(label({ label: 'grade-api' }), timestamp(), myFormat),
 });
